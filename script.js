899835958729bf4595dc05e15f3ac3f4b266412d
Ali Alkhateeb <36284011+vcpixhul@users.noreply.github.com>
2019-11-19 23:25:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+document.getElementById("Helvetica").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "Helvetica";
+}
+document.getElementById("Georgia").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "Georgia";
+}
+document.getElementById("Palatino Linotype").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "Palatino Linotype";
+}
+document.getElementById("Book Antiqua").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "Book Antiqua";
+}
+document.getElementById("Arial").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "Arial";
+}
+document.getElementById("Verdana").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "Verdana";
+}
+document.getElementById("serif").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "serif";
+}
+document.getElementById("sans-serif").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "sans-serif";
+}
+document.getElementById("fantasy").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "fantasy";
+}
+
+
+
+document.getElementById("monospace").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "monospace";
+}
+document.getElementById("cursive").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "cursive";
+}
+document.getElementById("system-ui").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "system-ui";
+}
+document.getElementById("fantasy").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "fantasy";
+}
+document.getElementById("emoji").onclick =function(){
+    document.getElementById("entry").style.fontFamily= "emoji";
+}
+
+
+
